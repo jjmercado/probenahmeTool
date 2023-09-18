c5d35ac7ceec254b13a6569b55699537bb549d07
Jeremy Mercado <jeremy.j.mercado@hotmail.com>
2023-09-18 16:14:32 +0200

    Add signature pad

--- src/routes/Signing.svelte
+++ src/routes/Signing.svelte
@@ -1,6 +1,36 @@
-<h1>Footer</h1>
+<script>
+    import lemonade from "lemonadejs";
+    import Signature from "@lemonadejs/signature";
+    import { onMount } from "svelte";
+ 
+    onMount(() => {
+        const signaturePad = document.getElementById("signaturePad");
+        
+        Signature(signaturePad, {
+        value: [],
+        width: 500,
+        height: 100,
+        instructions: "Please sign this document"
+    })
+
+});
+
+</script>
+
+<h1>Unterschriftfeld</h1>
 
 <label for="comments">Bemerkungen</label>
 <textarea name="comments" id="comments"></textarea>
 <input type="date" name="date">
 <!-- Unterschriftfeld -->
+<div id="signaturePad"></div>
+
+<style>
+    #signaturePad
+    {
+        border:1px dashed #ccc;
+        display: inline-block;
+        text-align: center;
+        margin-bottom: 10px;
+    }
+</style>
